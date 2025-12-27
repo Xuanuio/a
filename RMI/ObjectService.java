ad37fdc1dbb9e013182e7f3caa399c2174a400b6
Mai Hoàng Xuân <XuanMH.B22CN925@stu.ptit.edu.vn>
2025-12-27 21:14:14 +0700

    Delete RMI directory

--- RMI/ObjectService.java
+++
@@ -1,8 +0,0 @@
-package RMI;
-import java.rmi.*;
-import java.io.Serializable;
-
-public interface ObjectService extends Remote {
-    public Serializable requestObject(String studentCode, String qAlias) throws RemoteException;
-    public void submitObject(String studentCode, String qAlias, Serializable object) throws RemoteException;
-}
